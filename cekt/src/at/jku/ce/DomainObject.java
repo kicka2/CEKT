4eabc7b6e687ada1481bc840f07e3e77d21a3976
Barbara <thomas.steollner@aon.at>
2010-12-10 15:11:38 +0100

    Zurueck zu Tag1 Barbara

--- cekt/src/at/jku/ce/DomainObject.java
+++ cekt/src/at/jku/ce/DomainObject.java
@@ -41,25 +41,6 @@ public class DomainObject implements Comparable<DomainObject> {
 		return name;
 	}
 
-	public boolean equals (Object obj){
-		if(!(obj instanceof DomainObject)) {
-			return false;
-		}
-		DomainObject domainObj = (DomainObject) obj;
-		return uuid.equals(domainObj.getUuid());
-	}
-
-	public boolean hashcode(){
-		if(uuid != null){
-			return uuid.hashCode();
-		}else {
-			return super.hashCode();
-		}
-	public int compareTo(DomainObject o){
-		DomainObject domainObj = (DomainObject) o;
-		int domObjeComp = name.compareTo(domainObj.getName());
-		return ((domObjeComp == 0) ? uuid.compareTo(domainObj.getUuid()):domObjeComp);
-	}
-	}
+	
 
 }
