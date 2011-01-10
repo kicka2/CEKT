3bb2af06657b65c7e13cd983fea2042970e20706
unknown <thomas.stoellner@aon.at>
2011-01-10 16:25:40 +0100

    Reset Main

--- cekt/src/at/jku/ce/Main.java
+++ cekt/src/at/jku/ce/Main.java
@@ -1,26 +1,11 @@
 package at.jku.ce;
 
-private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-
 public class Main {
 
 	/**
 	 * @param args
 	 */
-
 	public static void main(String[] args) {
-	Set <DomainObject> objectSet = new TreeSet<DomainObject>(new DomainObjectComperator());
-
-	protected objectSet.add(new DomainObject ("Helga", "Kommentar1"));
-	protected objectSet.add(new DomainObject ("Walter", "Kommentar2"));
-	protected objectSet.add(new DomainObject ("Hannah", "Kommentar3"));
-	protected objectSet.add(new DomainObject ("Paul", "Kommentar4"));
-	protected objectSet.add(new DomainObject ("Claudia", "Kommentar5"));
-
-	for (DomainObject domainObject : objectSet)   {
-		LOGGER.debug(domainObject.toString());
-	}
-
 	}
 
 }
