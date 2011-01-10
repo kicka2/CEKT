7a49189761a93d268062468ea94829e705f300b4
unknown <thomas.stoellner@aon.at>
2011-01-10 18:28:02 +0100

    Aenderungen in Branch nach toString

--- cekt/src/at/jku/ce/Main.java
+++ cekt/src/at/jku/ce/Main.java
@@ -8,6 +8,11 @@ public class Main {
 	public static void main(String[] args) {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 	objectSet.add(new DomainObject("Helga", "hallo");
+
+
+	Anderung Hashset auf Treeset usw...
+
+	Logger mit toString ausgeben!
 	}
 
 änderung der ausgabe getuuid();
