de28de5d65c1c1d071a286d6b967321284808c5b
Artur <jakob.schroeger@hotmail.com>
2011-01-10 18:21:15 +0100

    l

--- cekt/src/at/jku/ce/Main.java
+++ cekt/src/at/jku/ce/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 	private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
-	objectSet.add(new DomainObject("Helga", "hallo");
+	objectSet.add(new DomainFactory.createDomainObject("Helga", "hallo");
 	}
 
 änderung der ausgabe getuuid();
